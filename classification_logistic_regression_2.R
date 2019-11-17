9d61230f19093fdbd4d9a1b5fdef99e5dd23a4a9
Rand0m-User-HSG <54806765+Rand0m-User-HSG@users.noreply.github.com>
2019-11-17 22:13:17 +0100

    added a demonstration of the alternative error calculation

--- classification_logistic_regression_2.R
+++ classification_logistic_regression_2.R
@@ -153,3 +153,51 @@ error_89_fold_cv = error_cv/89
 # Empirical_error_one_vs_all = 0.2163198
 # error_10_fold_cv = 0.2149383
 # error_89_fold_cv = 0.1966661
+
+
+#### trial 10-fold with different error calculation ####
+
+# in this section we use a different error calculation
+# each of the 10 beta matrices is used to calculate y_classified over the entire dataset
+# we thus produce 10 different errors
+# and pick the lowest one
+
+# code for cross-validation is based on the script Exercise4_Functions.R
+# JPO scripts -> day 2 -> cross-validation
+
+num_degrees = 3
+p = dim(X_matrix)[2]
+fold_10 = 10
+beta_one_vs_all = matrix(0,p + 1, num_degrees*fold_10)
+Empirical_error_10_fold = rep(0, fold_10) # we will store the 10 errors in this vector
+
+for (i in 1:fold_10) {
+  
+  yn = Y_vector[(1+(i-1)*5429):(i*5429)]
+  xn = X_matrix[(1+(i-1)*5429):(i*5429), ]
+  
+  for (d in 1:num_degrees) {
+    
+    degree_selected=which(yn==d)
+    y_d = yn
+    y_d[-degree_selected] = 0  
+    y_d[degree_selected] = 1
+    
+    data = data.frame(y_d,xn)
+    model_glmfit_d = glm(y_d ~., data, start =rep(0,p+1) ,family=binomial(link="logit"),
+                         control=list(maxit = 100, trace = FALSE) )
+    beta_glmfit_d  = model_glmfit_d$coefficients
+    beta_glmfit_d[is.na(beta_glmfit_d)]=0
+    
+    beta_one_vs_all[, d*i] = beta_glmfit_d 
+  }
+  
+  y_classified = apply(cbind(rep(1,nrow(X_matrix)), X_matrix) %*% beta_one_vs_all[, (1+(i-1)*3):(i*3)] , 1, FUN=which.max)
+  Empirical_error_10_fold[i] = length(which(y_classified != Y_vector)) / nrow(X_matrix)
+}
+
+error_10_fold_cv = min(Empirical_error_10_fold) # we pick the lowest error from the 10
+
+# the error with 10-fold cv is slightly worse than without 10-fold cv, and worse than with the first version of 10-fold cv
+# Empirical_error_one_vs_all = 0.2163198
+# error_10_fold_cv = 0.2219377
