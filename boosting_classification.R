8b0bca9ca53b18e3237f74a3dfb52989eae550c2
VDilello <57640496+VDilello@users.noreply.github.com>
2019-11-18 22:46:34 +0100

    Update boosting_classification.R

--- boosting_classification.R
+++ boosting_classification.R
@@ -1,5 +1,4 @@
 ###Introduction ###
-
 #In this script we perform a boosting for a classification task
 #We would like to classify the severity of an accident. 
 #They are three degrees of severity: light injuries, severe injuries and fatalities. 
@@ -8,17 +7,15 @@ rm(list=ls())
 library(tidyverse)
 library(dplyr)
 
-
----------------##First attempt with xgboost package##--------------------------
-#Firstly, we use the xgbboost package to try performing this classification problem. 
-
-library(xgboost)
-
 # we load the covariate matrix with dummy variables that was produced in covariate_matrix_with_dummy_variables.R
 load("./Data/covariate_matrix.RData") # the name of this matrix is X_matrix
 
 # we load the Y_vector that was produced in covariate_matrix_with_dummy_variables.R
 load("./Data/Y_vector_classification.RData") # the name of this vector is Y_vector
+
+---------------##First attempt with xgboost package##--------------------------
+#Firstly, we use the xgbboost package to try performing this classification problem. 
+library("xgboost")
 
 #The code for this script is based on the exercise 14_Boosting.R in R scripts JP
 
@@ -31,52 +28,89 @@ xgb.fit1 <- xgb.cv(
   nfold = 5,
   verbose = FALSE)
 
+
 #xgboost has a bug with multiclass classification, this is why we need to use another R package.
 
 -----------##Second attempt with gbm package##------------------------
 
 #This part of the code is based on the book "An Introduction to Statistical Learning" pp. 330-331
+
 library(gbm)
 
-#gbm requires a data frame and not a matrix
-X_df <- data.frame(X_matrix)
-Y_df <- data.frame(X_matrix)
+num_degrees = 3
+p = dim(X_matrix)[2]
+fold_10 = 10
+beta = matrix(0,p + 1, num_degrees)
+error = 0
 
-#Randomly shuffle the data
-X_df = X_df[sample(nrow(X_df)),]
-Y_vector = Y_df[sample(nrow(Y_df)),]
+for (i in 1:fold_10) {
+  
+  yn = Y_vector[(1+(i-1)*5429):(i*5429)]
+  xn = X_matrix[(1+(i-1)*5429):(i*5429),]
+  xn_test = X_matrix[-((1+(i-1)*5429):(i*5429)),]
+  
+  for (d in 1:num_degrees) {
+    
+    degree_selected=which(yn==d)
+    y_d = yn
+    y_d[-degree_selected] = 0  
+    y_d[degree_selected] = 1
+    
+    data = data.frame(y_d,xn)
+    data_test = data.frame(xn_test)
+    
+    model_gbmfit_d = gbm(y_d ~., data, distribution = "multinomial", n.trees = 100, interaction.depth = 4)
+  
+    pred <- predict.gbm(model_gbmfit_d, data_test, n.trees = 100, type="response")
+    
+    MSE[i] = (Y_vector[i] - pred[i])^2
+  }
+  Empirical_error_10_fold = MSE[i]
+  error = error + Empirical_error_10_fold }
 
-##-------Cross valation with k = 10 folds ------------------------
-#I try to perform the model with this one and when I get it I will try to do it with leave-one-out
-#cross-validation. 
+#I get the error message: 
+#Erreur : objet 'MSE' introuvable
+#De plus : Warning messages:
+#1: In gbm.fit(x = x, y = y, offset = offset, distribution = distribution,  :  variable 11: AI has no variation.
+#2: In gbm.fit(x = x, y = y, offset = offset, distribution = distribution,  : variable 12: AR has no variation.
 
-#Create 10 equally size folds
-folds = cut(seq(1,nrow(X_df)),breaks=10,labels=FALSE)
-folds_y = cut(seq(1,nrow (Y_df)),breaks=10,labels=FALSE)
+#and the pred function does not give class but probability, weird stuff
 
-#Perform 10 folds cross validation
-for(i in 1:10){
-  testIndexes <- which(folds==i,arr.ind=TRUE)
-  testData <- X_df[testIndexes, ]
-  trainData <- X_df[-testIndexes, ]
+----##Maboost##-----
+
+#maboost package is the extension of adaboost package for multiclass classification
+library(maboost)
+library(caret)
+
+num_degrees = 3
+p = dim(X_matrix)[2]
+fold_10 = 10
+beta = matrix(0,p + 1, num_degrees)
+error_cv = 0
+
+
+for (i in 1:fold_10) {
+  
+  yn = Y_vector[(1+(i-1)*5429):(i*5429)]
+  xn = X_matrix[(1+(i-1)*5429):(i*5429),]
+  xn_test = X_matrix[-((1+(i-1)*5429):(i*5429)),]
+  
+  for (d in 1:num_degrees) {
+    
+    degree_selected=which(yn==d)
+    y_d = yn
+    y_d[-degree_selected] = 0  
+    y_d[degree_selected] = 1
+    
+    data = data.frame(y_d,xn)
+    data_test = data.frame(xn_test)
+    
+    model_maboostfit_d = maboost(y_d ~., data)
+    
+    pred.x= predict(model_maboostfit_d,data_test,type="class");
+    
+  }
+  MSE[i] = ((Y_vector[i] - pred.x[i])^2)
+  empirical_error = error + MSE[i]
 }
 
-for(i in 1:10){
-  Yindexes <- which(folds_y==i,arr.ind=TRUE)
-  Ytest<- Y_df[Yindexes, ]
-  Ytrain <- Y_df[-Yindexes, ]
-}
-
-model_fit_train <- gbm(Ytrain ~., data = trainData, distribution = "multinomial", 
-                 n.trees = 100, interaction.depth = 4)
-
-#here is the new error message I get: Error in model.frame.default(formula = Ytrain ~ ., 
-#data = trainData, drop.unused.levels = TRUE,  : 
-#type (list) incorrect pour la variable 'Ytrain'
-
-
-#If I try to do it for X_matrix, without separating the training and testing data, I get
-#and it runs forever without giving any output. 
-
-model_fit <- gbm(Y_vector~., data = X_df, distribution = "multinomial", 
-                       n.trees = 100, interaction.depth = 4)
