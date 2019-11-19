62630751286d9b9896a11fec8b70df5e3a30c486
VDilello <57640496+VDilello@users.noreply.github.com>
2019-11-19 10:04:39 +0100

    Update boosting_class.R

--- boosting_class.R
+++ boosting_class.R
@@ -18,7 +18,7 @@ load("./Data/Y_vector_classification.RData")
 k = 10
 num_degrees <- unique(Y_vector)
 error_cv = rep(NA, k)
-pred.x = matrix(NA, nrow = nrow(X_matrix)/k, ncol = k)
+pred= matrix(NA, nrow = nrow(X_matrix)/k, ncol = k)
 y_classified <- rep(NA, length(Y_vector))
 
 for (i in 1:k) {
@@ -30,10 +30,10 @@ for (i in 1:k) {
     
   model_maboost = maboost(x = xn, y = yn, iter = 5, nu = .1, C50tree = T, C5.0Control(CF = .2, minCase = 128))
   
-  pred= predict(model_maboost,data_test,type="class");
+  pred= predict(model_maboost,xn_test,type="class");
 
   y_classified[(1+(i-1)*nrow(X_matrix)/k):(i*nrow(X_matrix)/k)] <- pred
-  error_cv[i] = mean(abs(yn_test - as.integer(pred.x)))
+  error_cv[i] = mean(abs(yn_test - as.integer(pred)))
 }
 
 # this is for the last loop only
