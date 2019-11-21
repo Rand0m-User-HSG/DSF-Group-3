6ad9d8a0b8e5841fa9e246dd26dda167fcc25bf3
KerimBenHalima <57640934+KerimBenHalima@users.noreply.github.com>
2019-11-21 21:21:46 +0100

    Big update

--- CNN_class_cv.R
+++ CNN_class_cv.R
@@ -28,7 +28,7 @@ model %>% compile(
 
 training <- model %>% fit(
   X_matrix, Y, 
-  epochs = 50, batch_size = 1024, 
+  epochs = 50, batch_size = 128, 
   validation_split = 0.2)
 
 pred <- model %>% predict_classes(X_matrix)
@@ -40,6 +40,13 @@ for (i in 2:ncol(Y)) {
   }
 }
 
+Beta <- model$get_weights()
+Beta_input <- Beta[[1]]
+Beta_input2 <- Beta[[2]]
+Beta_hidden <- Beta[[3]]
+Beta_hidden2 <- Beta[[4]]
+Beta_output <- Beta[[5]]
+Beta_output2 <- Beta[[6]]
 ############################################################## 10-fold cv now
 
 k = 10
@@ -91,6 +98,47 @@ misclassification_matrix = matrix(0, ncol(Y)-1, ncol(Y)-1)
 for (i in 2:ncol(Y)) {
   for (j in 1:(ncol(Y)-1)) {
     misclassification_matrix[j ,i-1] = length(which((Y[,i] == 1) & (y_classified == i-1))) / length(which((Y[,i] == 1)))
+  }
+}
+print(misclassification_matrix)
+
+
+###################################################################################
+
+sigmoid <- function(z) {
+  x = 1/(1 + exp(z))
+}
+
+SoftMax <- function (z){
+  x = exp(z) / sum(exp(z))
+}
+
+X <- cbind(rep(1, nrow(X_matrix)), X_matrix)
+Beta_input_true <- cbind(Beta_input2, t(Beta_input))
+
+A <- sigmoid(Beta_input_true %*% t(X))
+
+A <- t(A)
+A <- cbind(rep(1, nrow(A)), A)
+Beta_hidden_true <- cbind(Beta_hidden2, t(Beta_hidden))
+dim(Beta_hidden_true)
+dim(A)
+P <- sigmoid(Beta_hidden_true %*% t(A))
+
+P <- t(P)
+P <- cbind(rep(1, nrow(P)), P)
+Beta_output_true <- cbind(Beta_output2, t(Beta_output))
+
+Pred <- SoftMax(Beta_output_true %*% t(P))
+Pred <- t(Pred)
+
+y_classified <- apply(Pred, 1, FUN = which.max)
+
+Errors = mean(abs(Y_vector - (y_classified-1)))
+misclassification_matrix = matrix(0, ncol(Y)-1, ncol(Y)-1)
+for (i in 2:ncol(Y)) {
+  for (j in 1:(ncol(Y)-1)) {
+    misclassification_matrix[j ,i-1] = length(which((Y[,i] == 1) & (y_classified == i))) / length(which((Y[,i] == 1)))
   }
 }
 print(misclassification_matrix)
