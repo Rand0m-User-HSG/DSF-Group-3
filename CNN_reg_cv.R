0a8dd0f4a21c464c2f9c66c75cef651e72f604f2
KerimBenHalima <57640934+KerimBenHalima@users.noreply.github.com>
2019-11-21 21:22:34 +0100

    Big update

--- CNN_reg_cv.R
+++ CNN_reg_cv.R
@@ -1,13 +1,9 @@
 rm(list = ls())
-#install.packages("tensor")
-#install.packages("keras")
-#install.packages("tensorflow")
-#install.packages("Rcpp")
+
 library(tensor)
 library(tensorflow)
 library(keras)
 library(tidyverse)
-#install_tensorflow()
 
 load("./Data/covariate_matrix_reg.RData")
 load("./Data/Y_vector_regression.RData")
@@ -33,12 +29,18 @@ build_model <- function() {
 }
 
 model <- build_model()
-model %>% summary()
 training <- model %>% fit(
   X_matrix, Y_vector, 
-  epochs = 50, batch_size = 1024, 
+  epochs = 50, batch_size = 256, 
   validation_split = 0.2)
 
+Beta <- model$get_weights()
+Beta_input <- Beta[[1]]
+Beta_input2 <- Beta[[2]]
+Beta_hidden <- Beta[[3]]
+Beta_hidden2 <- Beta[[4]]
+Beta_output <- Beta[[5]]
+Beta_output2 <- Beta[[6]]
 ############################################################## 10-fold cv now
 
 k = 10
@@ -74,9 +76,9 @@ for (i in 1:k){
   
   training <- model %>% fit(
     X_k, Y_k, 
-    epochs = 20, batch_size = 1024,
+    epochs = 100, batch_size = 1024,
     validation.split = 0,
-    verbose = 0 )
+    verbose =  0)
   
   pred <- model %>% predict(X_test)
   cv_MAE[i] <- mean(abs(Y_test - pred))
@@ -88,3 +90,31 @@ for (i in 1:k){
 
 mean(cv_MAE)
 mean(cv_error)
+
+################################################################# Predicting with the trained weights
+
+relu = function(z) {
+  x = log(1 + exp(z))
+}
+
+X <- cbind(rep(1, nrow(X_matrix)), X_matrix)
+Beta_input_true <- cbind(Beta_input2, t(Beta_input))
+
+A <- tanh(Beta_input_true %*% t(X))
+
+A <- t(A)
+A <- cbind(rep(1, nrow(A)), A)
+Beta_hidden_true <- cbind(Beta_hidden2, t(Beta_hidden))
+dim(Beta_hidden_true)
+dim(A)
+P <- tanh(Beta_hidden_true %*% t(A))
+
+P <- t(P)
+P <- cbind(rep(1, nrow(P)), P)
+Beta_output_true <- cbind(Beta_output2, t(Beta_output))
+
+Pred <- relu(Beta_output_true %*% t(P))
+Pred <- t(Pred)
+
+MAE = mean(abs(Y_vector - round(Pred, 0)))
+print(MAE)
