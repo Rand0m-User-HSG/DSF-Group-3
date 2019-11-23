b32cabf9095a7457f56f077a2def47c707cb8f2f
Rand0m-User-HSG <54806765+Rand0m-User-HSG@users.noreply.github.com>
2019-11-23 15:11:11 +0100

    re-ordered the Y_vector and X_matrix so that their rows are ranked by date, from 2011 to 2018

--- REGRESSION_covariate_matrix_with_dummy_variables.R
+++ REGRESSION_covariate_matrix_with_dummy_variables.R
@@ -1,20 +1,31 @@
 #### Introduction ####
-# In this script we create a dataset specifically for regression tasks
-# The idea is to convert all categorical variables (e.g. road type) into dummy variables (1 or 0)
+# In this script we create a dataset specifically for the visualisation of regression tasks
+# The idea is twofold
+# (1) rank the rows by dates, so as to be able to plot time series of the number of accidents
+# (2) convert all categorical variables (e.g. road type) into dummy variables (1 or 0)
 # In doing so we avoid weigthing problems
 # e.g. we avoid that the model gives too much weight to the variable "road type" simply because this variable ranges from 1 to 6
 
 
 rm(list=ls())
 library(tidyverse)
-
+library(lubridate)
 
 # we load the dataset for regression tasks that was produced in Data_wrangling.R
 load("./Data/data_reg.RData") # this dataset is loaded as df_reg
 
 
+#### rank the dataset by dates ####
+# we first rank the dataset by dates, from the oldest to the most recent
+
+df_reg = df_reg %>% 
+  mutate(date = make_date(AccidentYear, AccidentMonth, days))
+
+df_reg = df_reg[order(df_reg$date),]
+
+
 #### Y_vector ####
-# we first create the Y_vector with the number of accidents in canton ZH on each day
+# we now create the Y_vector with the number of accidents in canton ZH on each day
 
 Y_vector = df_reg$number_accidents
 
@@ -47,7 +58,7 @@ X_matrix = X_matrix %>%
          October = ifelse(AccidentMonth == 10, 1, 0),
          November = ifelse(AccidentMonth == 11, 1, 0),
          December = ifelse(AccidentMonth == 12, 1, 0)) %>%
-  dplyr::select(10:21, 2:9)
+  dplyr::select(11:22, 2:10)
 
 # we then convert the weekdays into dummy variables (1 or 0)
 X_matrix = X_matrix %>%
@@ -58,7 +69,7 @@ X_matrix = X_matrix %>%
          Friday = ifelse(week_day_number == 5, 1, 0),
          Saturday = ifelse(week_day_number == 6, 1, 0),
          Sunday = ifelse(week_day_number == 7, 1, 0)) %>%
-  dplyr::select(1:14, 21:27, 16:20)
+  dplyr::select(1:14, 22:28, 16:21)
 
 # we then drop the column days, because it is not relevant for our predictions
 # indeed what matter is the month and the weekday, but not the day number in the month
@@ -69,8 +80,7 @@ X_matrix = X_matrix %>%
 X_matrix = X_matrix %>% 
   dplyr::select(-CantonCode)
 
-
-# we then standardize the last 5 columns (Temp, Pressure, Humidity, Prec_Amount, Wind_Spd)
+# we then standardize the 5 non-dummy columns (Temp, Pressure, Humidity, Prec_Amount, Wind_Spd)
 
 standFun = function(x){ #standFun is a function that standardizes -> mean = 0, sd = 1
   out = (x - mean(x))/sd(x)
@@ -82,6 +92,10 @@ for(i in 20:24){
 }
 
 # finally we convert the X_matrix (currently a dataframe) into a real matrix
-X_matrix = data.matrix(X_matrix)
+# and split it into a covariate matrix, and a date vector
+dates_regression = X_matrix$date
+X_matrix = data.matrix(X_matrix[, -25])
 
 save(X_matrix, file = "Data/covariate_matrix_reg.RData")
+save(dates_regression, file = "Data/dates_regression.RData")
+
