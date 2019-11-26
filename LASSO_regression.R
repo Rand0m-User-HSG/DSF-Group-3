a8c1fb16b4affe82f46f9672a4c74f3497ecc7c2
KerimBenHalima <57640934+KerimBenHalima@users.noreply.github.com>
2019-11-26 12:22:17 +0100

    now it works

--- LASSO_regression.R
+++ LASSO_regression.R
@@ -25,3 +25,33 @@ mod_cv <- cv.glmnet(x=X_matrix, y=Y_vector,   nfolds = nrows(X_matrix), family='
 # Find the best lambda
 mod_cv$lambda.1se
 
+# this works:
+####### LASSO regression #######
+
+rm(list = ls())
+load("./Data/covariate_matrix_reg.RData")
+load("./Data/Y_vector_regression.RData")
+
+
+library(tidyverse)
+library(glmnet)       # implementing regularized regression approaches
+
+
+
+# The code is based on the script of JPO, Day 1, Polynomial regression splines Ex_2_2_polynomial_regression_parallel.txt
+# install.packages("doParallel")
+library(iterators)
+library(parallel)
+library(doParallel)
+
+# Parallel computation
+cl = makeCluster(32)
+
+registerDoParallel(cl)
+
+mod_cv <- cv.glmnet(x=X_matrix, y=Y_vector, nfolds = nrow(X_matrix), family = "gaussian", alpha = 1)
+
+stopCluster(cl)
+
+# Find the best lambda
+mod_cv$lambda.1se
