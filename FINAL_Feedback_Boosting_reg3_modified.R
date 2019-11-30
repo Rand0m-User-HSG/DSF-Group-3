c65a26e064d050d6dc6444e840297915f6b6d642
VDilello <57640496+VDilello@users.noreply.github.com>
2019-11-30 17:25:42 +0100

    Update FINAL_Feedback_Boosting_reg3_modified.R

--- FINAL_Feedback_Boosting_reg3_modified.R
+++ FINAL_Feedback_Boosting_reg3_modified.R
@@ -25,72 +25,76 @@ load("./Data/covariate_matrix_reg.RData") # the name of this matrix is X_matrix
 load("./Data/Y_vector_regression.RData") # the name of this vector is Y_vector
 
 
-# The code for Parallel computation is based on the script of JPO, Day 1, Polynomial regression splines Ex_2_2_polynomial_regression_parallel.txt
-# Parallel computation
-cl = makeCluster(4)
-#registerDoSNOW(cl)
-registerDoParallel(cl)
 
 ####Generalized boosted regression model without cross validation####
+best_trees = 
+best_interaction_depth = 2
 
 #gbm requires data frames
 X_matrix <- data.frame(X_matrix)
 
-model_gbm = gbm(Y_vector  ~., data = X_matrix  ,distribution = "gaussian",cv.folds = 10,n.minobsinnode = 10,shrinkage = .01, n.trees = 3000)
+model_gbm = gbm(Y_vector  ~., data = X_matrix  ,distribution = "gaussian", 
+                n.minobsinnode = 10,shrinkage = .01, n.trees = best_trees, interaction.depth = best_interaction_depth)
 
-pred = predict.gbm(model_gbm, X_matrix ,n.trees = 3000, type = "link")
+pred = predict.gbm(model_gbm, X_matrix ,n.trees = best_trees, type = "link")
 
 yn_forecasted <- pred
 MSE_regression = sum((Y_vector - yn_forecasted)^2, na.rm = T)  / (length(Y_vector))
 MAE_regression = mean(abs(Y_vector - yn_forecasted), na.rm = T)
 
-# MSE_regression = 8.754159
-# MAE_regression = 2.308763
+# MSE_regression = 6.60857
+# MAE_regression = 2.013694
 
 ####10-folds cross-validation####
+best_trees = 
+best_interaction_depth = 2
 
-
-k = 10
+fold = 10
 sum_of_10_fold_cv_MSEs = 0
 sum_of_10_fold_cv_MAEs = 0
 
-yn_forecasted =  rep(NA, length(Y_vector)/k)
+yn_forecasted =  rep(NA, length(Y_vector)/fold)
 
-for (i in 1:k) {
+for (i in 1:fold) {
   
   
-  yn = Y_vector[-((1+(i-1)*nrow(X_matrix)/k):(i*nrow(X_matrix)/k))]
-  xn = data.frame(X_matrix[-((1+(i-1)*nrow(X_matrix)/k):(i*nrow(X_matrix)/k)),])
-  xn_test = data.frame(X_matrix[(1+(i-1)*nrow(X_matrix)/k):(i*nrow(X_matrix)/k),])
-  yn_test = Y_vector[(1+(i-1)*nrow(X_matrix)/k):(i*nrow(X_matrix)/k)]
+  yn = Y_vector[-((1+(i-1)*nrow(X_matrix)/fold):(i*nrow(X_matrix)/fold))]
+  xn = data.frame(X_matrix[-((1+(i-1)*nrow(X_matrix)/fold):(i*nrow(X_matrix)/fold)),])
+  xn_test = data.frame(X_matrix[(1+(i-1)*nrow(X_matrix)/fold):(i*nrow(X_matrix)/fold),])
+  yn_test = Y_vector[(1+(i-1)*nrow(X_matrix)/fold):(i*nrow(X_matrix)/fold)]
   
-  model_gbm = gbm(yn  ~., data = xn  ,distribution = "gaussian",n.minobsinnode = 10,shrinkage = .01, n.trees = 3000)
+  model_gbm = gbm(yn  ~., data = xn ,distribution = "gaussian",n.minobsinnode = 10,shrinkage = .01, 
+                  n.trees = best_trees, interaction.depth = best_interaction_depth)
+
   
-  pred = predict.gbm(model_gbm, xn_test, n.trees = 3000 ,type = "link")
+  pred = predict.gbm(model_gbm, xn_test, n.trees = best_trees ,type = "link")
   
   yn_forecasted <- pred
   sum_of_10_fold_cv_MSEs = sum_of_10_fold_cv_MSEs + sum((yn_test - yn_forecasted)^2, na.rm = T)  / (length(yn_test))
   sum_of_10_fold_cv_MAEs = sum_of_10_fold_cv_MAEs + mean(abs(yn_test - yn_forecasted), na.rm = T)
 }
 
-MSE_10_fold_cv = sum_of_10_fold_cv_MSEs / k
-MAE_10_fold_cv = sum_of_10_fold_cv_MAEs / k
+MSE_10_fold_cv = sum_of_10_fold_cv_MSEs / fold
+MAE_10_fold_cv = sum_of_10_fold_cv_MAEs / fold
 
-# MSE_10_fold_cv = 9.395641
-# MAE_10_fold_cv = 2.391612
+# MSE_10_fold_cv = 
+# MAE_10_fold_cv = 
 
 # Comparison to the errors without cross-validation
-# MSE_regression = 8.754159
-# MAE_regression = 2.308763
+# MSE_regression = 6.607747
+# MAE_regression = 2.012888
 # Getting slightly higher cv errors is normal, because they correspond to "testing" errors
 # unlike the errors from the previous section which were merely "training" errors
 
 ####Leave-one-out cross-validation####
-
+best_trees =
+best_interaction_depth = 2
 
 # Parallel computation
 library(foreach)
 library(doParallel)
+
+X_matrix <- data.frame(X_matrix)
 
 index=c(1:nrow(X_matrix))
 
@@ -99,7 +103,7 @@ cl = makeCluster(4)
 registerDoParallel(cl)
 
 start.time <- Sys.time()
-sse_cv = foreach (i =  1:nrow(X_matrix), .combine = rbind) %dopar% { 
+se_cv = foreach (i =  1:nrow(X_matrix), .combine = rbind) %dopar% { 
   indexn = index[-i]
   
   yn = Y_vector[indexn]
@@ -108,13 +112,17 @@ sse_cv = foreach (i =  1:nrow(X_matrix), .combine = rbind) %dopar% {
   library(gbm)
   
   fitresult_cv = gbm(yn  ~., data = xn  ,distribution = "gaussian", shrinkage = .01, 
-                     n.minobsinnode = 10, n.trees = best_trees)
+                     n.minobsinnode = 10, interaction.depth = best_interaction_depth, n.trees = best_trees)
   
-  (Y_vector[i] - predict.gbm(fitresult_cv, X-matrix[i, ], n.trees = best_trees, type = "link"))^2 
+  (Y_vector[i] - predict.gbm(fitresult_cv, X_matrix[i, ], n.trees = best_trees, type = "link"))^2 
 }
+
 mse_cv_LOO = sum(se_cv) / nrow(X_matrix)
 
-#mse_cv_LOO = ?
+mae_cv_LOO = mean((se_cv)^0.5)
+
+# mse_cv_LOO = 
+# mae_cv_LOO = 
 
 end.time <- Sys.time()
 (time_parallel_computation = end.time - start.time)
@@ -130,30 +138,40 @@ optim_boosting_cv<- function(depth, trees, X, Y){
   Y <- as.double(Y)
   
   model_gbm = gbm(Y ~., data = X, distribution = "gaussian", shrinkage = .01, 
-                  n.trees = trees, n.minobsinnode = 10, interaction.depth = depth)
+                  n.trees = trees, n.minobsinnode = 10, interaction.depth = depth, cv.folds = 10)
   
   pred <- model_gbm$fit
   
   return(mean(abs(Y_vector - pred)))
 }
 
-optim_boosting_cv(3000, X_matrix, Y_vector)
-optim_boosting_cv(4000, X_matrix, Y_vector)
-optim_boosting_cv(5000, X_matrix, Y_vector)
-optim_boosting_cv(6000, X_matrix, Y_vector)
-optim_boosting_cv(7000, X_matrix, Y_vector)
-optim_boosting_cv(8000, X_matrix, Y_vector)
-optim_boosting_cv(9000, X_matrix, Y_vector)
-optim_boosting_cv(10000, X_matrix, Y_vector)
+optim_boosting_cv(2,3000, X_matrix, Y_vector)
+# 2.196196
+optim_boosting_cv(2, 4000, X_matrix, Y_vector)
+# 2.16096
+optim_boosting_cv(2, 5000, X_matrix, Y_vector)
+# 2.130191
+optim_boosting_cv(2, 6000, X_matrix, Y_vector)
+# 2.102685
+optim_boosting_cv(2, 7000, X_matrix, Y_vector)
+# 2.079182
+optim_boosting_cv(2, 8000, X_matrix, Y_vector)
+# 2.057501
+optim_boosting_cv(2, 9000, X_matrix, Y_vector)
+# 2.03341
+optim_boosting_cv(2, 10000, X_matrix,Y_vector)
+#2.013958
 
 possible_depth_opt_cv <- 1:2
-possible_trees_opt_cv <- 1000:1001
+possible_trees_opt_cv <- 9900:10100
+#These values were optimzed because it was written on the help document that a shrinkage from 0.001 and 0.1
+#with a n.trees combination from 3000 until 10000 values was optimal
 
 row = 0 
 col = 0
 error_opt_cv <- matrix(NA, nrow = length(possible_trees_opt_cv), ncol = length(possible_depth_opt_cv))
 
-for (k in possible_depth_opt_cv){
+for (fold in possible_depth_opt_cv){
   col = col +1
   row = 0
   for (j in possible_trees_opt_cv){
@@ -164,9 +182,8 @@ for (k in possible_depth_opt_cv){
 
 best_parameters_opt_cv<- which(error_opt_cv == min(error_opt_cv), arr.ind = T) # remember to adjust it based on the start of the possible_trees (and depth)
 print(best_parameters_opt_cv)
-#   2   2
-#interaction.depth = 2, n.trees = 1001
+#   
+#interaction.depth = 2, n.trees = 
 print(min(error_opt_cv))
 
-
-#error_opt_cv = 2.302519
+#error_opt_cv = 
